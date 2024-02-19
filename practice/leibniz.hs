8669d39e43872e26bcc14a86ce148cbf4da0f77d
t-takezaki <49390028+Tomoyuki-TAKEZAKI@users.noreply.github.com>
2024-02-19 22:34:00 +0900

    chapter 13

---
+++ practice/leibniz.hs
@@ -0,0 +1,14 @@
+estPi :: Int -> Double
+estPi = (4 *) . estPi4
+
+estPi4 :: Int -> Double
+estPi4 0 = 1
+estPi4 n = estPi4 (n - 1) + (-1) ^ n / (2 * fromIntegral n + 1)
+
+
+estPi' :: Int -> Double
+estPi' n =  (4 *) . sum . take n $ map (\k -> (-1) ** k / (2 * k + 1)  ) [0 ..]
+
+sumN :: Integer -> Integer
+sumN 0 = 0
+sumN n = n + sumN (n - 1)
